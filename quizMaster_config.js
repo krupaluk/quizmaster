67ef57c88a564aa0150a4867ea859fc5f9ee6ff9
Lukas Krupa <krupa.luke@gmail.com>
2026-04-06 17:28:20 +0200

    wiki_icon_fix

--- quizMaster_config.js
+++ quizMaster_config.js
@@ -7,16 +7,19 @@ const quizCategories = {
                 id: "prijimacky_ff_ou",
                 title: "Otázky z přijímaček FF OU",
                 desc: "Oficiální otázky z přijímaček FF OU z období 2018-2024 rozdělené podle témat.",
+                wiki: false
             },
             pocatkyPoVrcholneReckeObdobi: {
                 id: "pocatky_po_vrcholne_recke_obdobi",
                 title: "Od počátků k vrcholu řecké filosofie",
                 desc: "Od mýtů k logu, přes hledání pralátky (arché) až po vrcholné systémy Platóna a Aristotela.",
+                wiki: true
             },            
             helenismus: {
                 id: "obdobi_helenismu",
                 title: "Období Helénismu",
                 desc: "Filosofická cesta za vnitřní svobodou v rozbouřeném světě skrze praktickou etiku stoiků, epikurejců a skeptiků.",
+                wiki: true
             }
         }
     },
@@ -28,26 +31,31 @@ const quizCategories = {
                 id: "dejiny_a_predstavitele",
                 title: "Dějiny logiky",
                 desc: "Klíčová období a představitelé. Od Aristotelových sylogismů přes Leibnizův sen o univerzálním jazyce až po Gödelovy věty.",
+                wiki: true
             },
             teorieLogiky: {
                 id: "teorie_logiky",
                 title: "Teorie Logiky",
                 desc: "Od pojmů a definic přes logické operace až po úsudky, argumentaci a odhalování klamů.",
+                wiki: true
             },
             logickeHadanky: {
                 id: "logicke_hadanky",
                 title: "Logické hádanky",
                 desc: "První krok k řešení logických úloh formou přirozeného jazyka. Od sylogismů přes logické spojky, klamy, negace až po analytické hádanky a formalizaci logiky.",
+                wiki: true
             },
             vyrokovaLogika: {
                 id: "vyrokova_logika",
                 title: "Výroková Logika",
                 desc: "Naučíte se číst logické zápisy (¬, ∧, ∨, →), používat De Morganovy zákony a provádět ekvivalentní úpravy.",
+                wiki: true
             },
             predikatovaLogika: {
                 id: "predikatova_logika",
                 title: "Predikátová Logika",
                 desc: "Pronikněte do tajů kvantifikátorů (∀, ∃), pochopte kritické rozdíly v jejich pořadí a naučte se precizně negovat i ty nejsložitější věty.",
+                wiki: true
             },
         }
     },
@@ -59,6 +67,7 @@ const quizCategories = {
                 id: "zvirata_na_statku",
                 title: "Zvířata na statku",
                 desc: "Poznejte zvířátka ze statku, zjistěte, jak se jmenují jejich mláďata, co dobrého papají a jaké zajímavé zvuky vydávají!",
+                wiki: false
             }
         }
     },
@@ -70,11 +79,13 @@ const quizCategories = {
                 id: "debug_3q",
                 title: "Debug 3 otázky",
                 desc: "Testovací kvíz se 3 jednoduchými otázkami pro ladění aplikace",
+                wiki: false
             },
             debug5q: {
                 id: "debug_5q",
                 title: "Debug 5 otázek",
                 desc: "Testovací kvíz se 5 jednoduchými otázkami pro ladění aplikace",
+                wiki: false
             }
         }
     }    
